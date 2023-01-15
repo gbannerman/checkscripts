6d36b55971f16eb98d14ebb3f5b0c3bf0da0579d
Gavin Bannerman <16233190+gbannerman@users.noreply.github.com>
2023-01-15 18:33:09 +0000

    Change `waitForInput` to reference unicode instead of hex

--- src/lib/input.ts
+++ src/lib/input.ts
@@ -1,12 +1,12 @@
-const EXIT_KEYS = ["03", "1b"];
-const CONTINUE_KEYS = ["20"];
+const EXIT_KEYS = ["\u001B", "\u0003"];
+const CONTINUE_KEYS = ["\u0020"];
 
 export async function waitForInput() {
   process.stdin.setRawMode(true);
   return new Promise<void>((resolve) =>
     process.stdin.on("data", (data) => {
-      const shouldContinue = CONTINUE_KEYS.includes(data.toString("hex"));
-      const shouldExit = EXIT_KEYS.includes(data.toString("hex"));
+      const shouldContinue = CONTINUE_KEYS.includes(data.toString("utf8"));
+      const shouldExit = EXIT_KEYS.includes(data.toString("utf8"));
       if (shouldContinue) {
         process.stdin.setRawMode(false);
         process.stdin.removeAllListeners("data");
